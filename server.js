6d0a0d8cca30dda2eccf44f98aee9165f3f5c434
Ranya Roshdy <ranya.roshdy97@gmail.com>
2020-05-15 22:43:06 +0200

    change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require("cors");
+
 app.use(express.static(__dirname + '/dist/project'));
 
 
