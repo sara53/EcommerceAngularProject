ea41ecfaadd9c781996281e01e7c4cfac7d65e7a
Ranya Roshdy <ranya.roshdy97@gmail.com>
2020-05-15 22:01:58 +0200

    commit

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require("cors");
-app.use(cors());
-app.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
 app.use(express.static(__dirname + '/dist/project'));
 
 
